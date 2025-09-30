177bf83f82774b52d52d9498bc22a59d7e75b408
gevalinho <gevalinho1@gmail.com>
2025-09-30 09:28:08 +0100

    Importing Google fonts

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
+      <h1> Welcome to Splash App </h1>
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
         Welcome to Splash App!
       </h1>
